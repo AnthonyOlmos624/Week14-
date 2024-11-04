da223ce2c7dd8e97fa4c87aac4608d8f4110668d
Anthony Olmos <anthony42olmos@gmail.com>
2024-11-03 19:38:10 -0800

    got rid of a few things that was no longer needed here but added deleteItem, and updateQuantity here along with buttons of delete and quantity

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -1,23 +1,22 @@
-//Define the type for an item
-interface Item {
-    id: number;
-    name: string;
-    price: number;
-    quantity: number;
-}
+import React from "react";
+import { Item } from './testData'
 
-//Should define the props for ItemCard where 'item' is of type Item
+//Should define the props for ItemCard where 'item' is of type Item. Adding delete and update function so that it can accept the prop 
 interface ItemCardProps {
     item: Item;
+    deleteItem: (id: number) => void;
+    updateQuantity: (id: number, newQuantity: number) => void 
 }
 
-function ItemCard ({ item }: ItemCardProps) {
+function ItemCard ({ item, deleteItem, updateQuantity }: ItemCardProps) {
     return(
         <div className="ItemCard">
             <h3>{item.name}</h3>
             <p>Price: ${item.price}</p>
             <p>Quantity: {item.quantity}</p>
-            {/* Might add buttons here to add/remove/ update items */}
+            <button onClick={() => deleteItem(item.id)}>Delete</button>
+            <button onClick={() => updateQuantity(item.id, Math.max(0, item.quantity + 1))}>Increase</button>
+            <button onClick={() => updateQuantity(item.id, Math.max(0, item.quantity - 1))}>Decrease</button>
         </div>
     );
 }
