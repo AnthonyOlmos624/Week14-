1f895c4a113e49fd5fab9714354496f6d3250043
Anthony Olmos <anthony42olmos@gmail.com>
2024-11-03 19:40:53 -0800

    added to my ItemList of deleteItem, and updateQuantity

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -1,24 +1,27 @@
+import React from "react";
 import ItemCard from "./ItemCard";
+import { Item } from './testData'
 
-//Need to define the type for each item
-interface Item {
-    id: number;
-    name: string;
-    price: number;
-    quantity: number;
-}
 
-//need to define the props for ItemList ensuring the 'items' prop is an array of Item
+//need to define the props for ItemList ensuring the 'items' prop is an array of Item. Need to add prop to pass deleteItem and updateQuantity
 
 interface ItemListProps {
     items: Item[];
+    deleteItem: (id: number) => void;
+    updateQuantity: (id: number, newQuantity: number) => void;
 }
 
-function ItemList({ items }: ItemListProps) {
+function ItemList({ items, deleteItem, updateQuantity }: ItemListProps) {
     return (
         <div>
             {items.map((item) => (
-                <ItemCard key={item.id} item={item} />
+                <ItemCard 
+                    key={item.id} 
+                    item={item}
+                    deleteItem={deleteItem}
+                    updateQuantity={updateQuantity} 
+                    
+                />
             ))}
         </div>
     );
