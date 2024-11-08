30cd88063d75ee59517b16ea203ab99668d3b984
Anthony Olmos <anthony42olmos@gmail.com>
2024-11-08 14:13:55 -0800

    added a button for filters so that it can be ordered low-to-high or high-to-low

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,14 +1,17 @@
 
 //creating the sidebar functions to be used by user. Now they should be able to filter items by price
 interface SidebarProps {
-    filterItemsByPrice: () => void
+    setSortOrder: (order: 'low-to-high' | 'high-to-low' | 'default') => void;
+    filterItemsByPrice: () => void;
 }
 
-function Sidebar({ filterItemsByPrice}: SidebarProps) {
+function Sidebar({ setSortOrder, filterItemsByPrice }: SidebarProps) {
     return (
         <aside className="Sidebar">
             <h2>Filters</h2>
             {/*could add more filter options here  */}
+            <button onClick={() => setSortOrder("low-to-high")}>Sort by Price: Low to High</button>
+            <button onClick={() => setSortOrder("high-to-low")}>Sort by Price: High to Low</button>
             <button onClick={filterItemsByPrice}>Filter by Price</button>
             
         </aside>    
